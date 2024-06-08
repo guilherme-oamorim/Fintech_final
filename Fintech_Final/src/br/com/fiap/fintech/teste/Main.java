12d802a37e3338fe0cf4e242bcde1bfcf33868e5
Lucas Montagnana <lucas.freitas1998@outlook.com>
2024-06-07 22:05:30 -0300

    Commit manutenção classe Login

--- Fintech_Final/src/br/com/fiap/fintech/teste/Main.java
+++ Fintech_Final/src/br/com/fiap/fintech/teste/Main.java
@@ -5,20 +5,33 @@ import java.util.List;
 import br.com.fiap.fintech.bean.Categoria;
 import br.com.fiap.fintech.bean.Investimento;
 import br.com.fiap.fintech.bean.Login;
-import br.com.fiap.fintech.bean.Perfil;
 import br.com.fiap.fintech.bean.Transacao;
 import br.com.fiap.fintech.dao.impl.OracleCategoriaDAO;
 import br.com.fiap.fintech.dao.impl.OracleInvestimentoDAO;
 import br.com.fiap.fintech.dao.impl.OracleLoginDAO;
-import br.com.fiap.fintech.dao.impl.OraclePerfilDAO;
 import br.com.fiap.fintech.dao.impl.OracleTransacaoDAO;
 
+import java.sql.Date;
 import java.time.LocalDate;
 
 public class Main {
 	public static void main(String[] args) {
 		
-		//classe de testes geral
+		OracleLoginDAO loginDAO = new OracleLoginDAO();
+		Login login = new Login();
+		Main.inserirLogin(loginDAO, login);
+		
+		
+	}
+	
+	public static void inserirLogin(OracleLoginDAO dao, Login login) {
 
+		login.setNm_login("Marco");
+		login.setDs_email("marcopolo@gmail.com");
+		login.setDs_senha("marcopolo");
+		login.setVl_saldo(1000);
+		login.setDt_criacao(LocalDate.now());
+
+		dao.Cadastrar(login);
 	}
 }
--- Fintech_Final/src/br/com/fiap/servlets/loginServlet.java
+++ Fintech_Final/src/br/com/fiap/servlets/loginServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class loginServlet
  */
-@WebServlet("/loginServlet")
+@WebServlet("/login")
 public class loginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
